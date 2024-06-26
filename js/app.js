8c89a4de11d2ab0986910cc1ce86bfd2cd9ba6c5
João Barboza <jprsbarboza@gmail.com>
2024-06-26 19:29:25 -0300

    adicionar confirmarDevolucao/numJogosAlugados

--- js/app.js
+++ js/app.js
@@ -1,17 +1,27 @@
+let numJogosAlugados = 1;
+
 function alterarStatus(num) {
   let botaoClicado = document.getElementById(`game-${num}`);
   let fotoJogo = botaoClicado.querySelector(".dashboard__item__img");
   let botaoJogo = botaoClicado.querySelector(".dashboard__item__button");
 
   if (fotoJogo.classList.contains("dashboard__item__img--rented")) {
-    fotoJogo.classList.remove("dashboard__item__img--rented");
-    botaoJogo.classList.remove("dashboard__item__button--return");
-    botaoJogo.innerHTML = "Alugar";
+    confirmarDevolucao = window.confirm(
+      "Você realmente deseja devolver esse item?"
+    );
+    if (confirmarDevolucao == true) {
+      fotoJogo.classList.remove("dashboard__item__img--rented");
+      botaoJogo.classList.remove("dashboard__item__button--return");
+      botaoJogo.innerHTML = "Alugar";
+      numJogosAlugados--;
+    }
   } else {
     fotoJogo.classList.add("dashboard__item__img--rented");
     botaoJogo.classList.add("dashboard__item__button--return");
     botaoJogo.innerHTML = "Devolver";
+    numJogosAlugados++;
   }
+  console.log(`A quantidade de jogos alugados é ${numJogosAlugados}`);
 }
 
 //querySelector é utilizado para selecionar elementos HTML com base em seu id, classes, tipos, atributos e etc...
